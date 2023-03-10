8e8219dfe1a84eb7b6e16fb0a2534cf4934113f1
Bruno Costa <bruno@rhizom.me>
2023-03-10 16:12:50 -0300

    first commit

---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+module.exports = {
+  stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
+  ],
+  typescript: {
+    check: true, // type-check stories during Storybook build
+  }
+};
